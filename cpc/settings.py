bcd4f4dbc99a4911a7fdf60814a5b356f9e961ad
nurkyz111 <bektawovanurkyz@gmail.com>
2021-05-05 18:38:30 +0600

    added allowed hosts

--- cpc/settings.py
+++ cpc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%5y&3(07zv4h$ghn)3415y#j_i(tz%+9n3po2es+mmgaey3qvu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
